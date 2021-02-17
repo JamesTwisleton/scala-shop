631efdec9912708550efbe8f255a11bb2661ce53
= <ajtwisleton@gmail.com>
2021-02-17 16:39:50 +0000

    BuyableItem trait, Apple and Orange case objects

--- src/main/scala/Shop.scala
+++ src/main/scala/Shop.scala
@@ -1,6 +1,16 @@
 object Shop extends App {
-  helloWorld
-  def helloWorld: String = {
-    "hello world"
+  trait BuyableItem {
+    def name: String
+    def price: BigDecimal
+  }
+
+  case object Apple extends BuyableItem {
+    val name = "Apple"
+    val price = 0.6
+  }
+
+  case object Orange extends BuyableItem {
+    val name = "Orange"
+    val price = 0.25
   }
 }
--- src/test/scala/ShopTest.scala
+++ src/test/scala/ShopTest.scala
@@ -1,5 +1,16 @@
+import Shop._
 class ShopTest extends org.scalatest.funsuite.AnyFunSuite {
-  test("Shop.helloWorld") {
-    assert(Shop.helloWorld === "hello world")
+  test("Apple case object has name \"Apple\"") {
+    assert(Apple.name === "Apple")
+  }
+  test("Apple case object has price 0.6") {
+    assert(Apple.price === 0.6)
+  }
+
+  test("Orange case object has name \"Orange\"") {
+    assert(Orange.name === "Orange")
+  }
+  test("Orange has price 0.25") {
+    assert(Orange.price === 0.25)
   }
 }
